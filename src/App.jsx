bb183431351219242e21ba1f919783b8bd0ae477
Arjun Rai <arai88292@gmail.com>
2025-05-09 12:48:08 +0530

    navlinks added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
-  return <div>World Wise App</div>;
+  return (
+    <div>
+      <h1>World Wise</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="homepage" element={<Homepage />} />
+          <Route path="pagenotfound" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
